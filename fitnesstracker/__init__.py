c97c70a842a05c56720fe3a35056fc7d01533d52
chavias <matias.chavez@mail.ch>
2025-01-22 18:36:06 +0100

    add docker-compose for debug and production

--- fitnesstracker/__init__.py
+++ fitnesstracker/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='34110d22dfd1e0ac02ddca391a1db27f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+
+db = SQLAlchemy(app)
 from fitnesstracker import routes
--- run.py
+++ run.py
@@ -2,10 +2,12 @@ from fitnesstracker import app, db
 
 
 if __name__ == "__main__":
+
     with app.app_context():
         db.create_all()
         # from sqlalchemy import inspect
         # inspector = inspect(db.engine)
         # tables = inspector.get_table_names()
         # print(f"{tables}")
+
     app.run(debug=True, host="0.0.0.0")
