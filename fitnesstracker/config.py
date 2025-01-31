752e040f9ef812f2efd831d4febea76419bdaa35
chavias <matias.chavez@mail.ch>
2025-01-31 18:18:26 +0100

    add .env.example

--- fitnesstracker/config.py
+++ fitnesstracker/config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '6f1d9450164dda48f7fac81a7f4a89c5bff52d841050b71897b6da8c3a46e5fd')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
 
 class DevelopmentConfig(Config):
