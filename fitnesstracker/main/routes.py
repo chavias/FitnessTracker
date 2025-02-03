9ab0253c3ce8a41b5bfa92f852559ee56c74139f
chavias <matias.chavez@mail.ch>
2025-02-03 13:08:12 +0100

    add pagination to homepage

--- fitnesstracker/main/routes.py
+++ fitnesstracker/main/routes.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from fitnesstracker.models import TrainingSession
-
 
 main = Blueprint('main', __name__)
 
@@ -8,5 +7,6 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 @main.route("/")
 def homepage():
-    sessions = TrainingSession.query.order_by(TrainingSession.id.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    sessions = TrainingSession.query.order_by(TrainingSession.date.desc()).paginate(page=page, per_page=5)
     return render_template("index.html", sessions=sessions)
