b39be84a642f68ec730c9b9d8b5f97813dd02a6a
chavias <matias.chavez@mail.ch>
2025-02-21 15:03:34 +0100

    initialize Bcrypt, Mail and LoginManager in fitnesstracker/__init__.py

--- fitnesstracker/__init__.py
+++ fitnesstracker/__init__.py
@@ -3,11 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from fitnesstracker.config import DevelopmentConfig, ProductionConfig, TestingConfig
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
 
 load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
 
 def create_app(enviroment='development'):
 
@@ -23,6 +34,10 @@ def create_app(enviroment='development'):
     # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
 
     @app.before_request
     def make_session_permanent():
