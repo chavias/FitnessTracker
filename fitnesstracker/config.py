772a39769d53d625dc8525622ff2c272f2656751
chavias <matias.chavez@mail.ch>
2025-02-25 15:59:45 +0100

    update db migration script for mariadb

--- fitnesstracker/config.py
+++ fitnesstracker/config.py
@@ -3,15 +3,15 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '6f1d9450164dda48f7fac81a7f4a89c5bff52d841050b71897b6da8c3a46e5fd')
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False).lower() in ('false', '0', 'f')
+    SQLALCHEMY_TRACK_MODIFICATIONS = str(os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False')).lower() in ('false', '0', 'f')
     WTF_CSRF_TIME_LIMIT = None
     PERMANENT_SESSION_LIFETIME = timedelta(hours=12)
     MAIL_USERNAME=os.getenv('EMAIL_USER')
     MAIL_PASSWORD=os.getenv('EMAIL_PASS')
     MAIL_SERVER=os.getenv('MAIL_SERVER')
     MAIL_PORT=os.getenv('MAIL_PORT')
-    MAIL_USE_TLS=os.getenv('TLS').lower() in ('true', '1', 't')
-    MAIL_USE_SSL=os.getenv('SSL').lower() in ('true', '1', 't')
+    MAIL_USE_TLS=str(os.getenv('TLS')).lower() in ('true', '1', 't')
+    MAIL_USE_SSL=str(os.getenv('SSL')).lower() in ('true', '1', 't')
     MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER')
 
 
